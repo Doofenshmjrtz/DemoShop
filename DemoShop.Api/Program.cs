d67c77ecacd0542e3ac611e9be0dab71cb9e24cb
Doofenshmjrtz <beqagvritishvili@gmail.com>
2025-03-22 13:04:16 +0400

    repository update

--- DemoShop.Api/Program.cs
+++ DemoShop.Api/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddControllers(options =>
 builder.Services.AddDbContext<DemoShopDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Register Unit of Work and Repositories
+// Register Unit of Work and Repositories   
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.Scan(scan => scan
@@ -59,28 +59,4 @@ app.MapControllers();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- DemoShop.Infrastructure/Contracts/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-using DemoShop.Domain.Core.Common.Abstractions;
-
-namespace DemoShop.Infrastructure.Contracts;
-
-public interface IUnitOfWork : IDisposable
-{
-    IRepository<T> GetRepository<T>() where T : Entity;
-    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
-}
